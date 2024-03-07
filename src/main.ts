9497c62f40a1ad2688dca102713ed0e6645b9fc7
Shuuhei Igarashi <shuuheiigarashi@USERnoMacBook-Pro.local>
2024-03-07 15:27:37 +0900

    graphQLを導入する

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { logger } from './common/middleware/logger.middleware';
-import { HttpExceptionFilter } from './common/http-exception.filter';
-import { AllExceptionsFilter } from './common/all-exceptions.filter';
 import { ValidationPipe } from './common/validation.pipe';
-import { RolesGuard } from './common/roles/roles.guard';
 import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalGuards(new RolesGuard());
   app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 }
