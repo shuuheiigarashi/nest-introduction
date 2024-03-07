19246ff61957f12639cf04675673d503e2cec61b
Shuuhei Igarashi <shuuheiigarashi@USERnoMacBook-Pro.local>
2024-03-07 16:24:38 +0900

    module等を作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { RecipesModule } from './recipes/recipes.module';
+import { RecipesResolver } from './recipes/recipes.resolver';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: 'schema.graphql',
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    RecipesModule,
   ],
+  providers: [RecipesResolver],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './common/validation.pipe';
-import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
